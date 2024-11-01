642a4a011a70f75178febb2dbc10a18af1e91210
Jan Dobes <dobesjan97@gmail.com>
2024-11-01 21:09:05 +0100

    task controller

---
+++ TestManagement/TestManagement.Api/Controllers/TaskController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestManagement.Api.Controllers.Abstract;
+using TestManagement.DataAccess.Repository.Tasks;
+
+namespace TestManagement.Api.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TaskController : WriteController<Models.Tasks.Task>
+	{
+		public TaskController(ITaskRepository taskRepository) : base(taskRepository) { }
+	}
+}
